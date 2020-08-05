79eba7f4b25c384b0e52e54d4fcb761152138b4c
Aki-373 <kusamoch@gmail.com>
2020-08-05 12:49:21 +0900

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by 甲田明寛 on 2020/08/05.
+//  Copyright © 2020 Akihiro Koda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
